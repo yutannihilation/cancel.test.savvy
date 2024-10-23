85838fb95b30a966e088e58a7ab17a174bdfb996
Hiroaki Yutani <yutani.ini@gmail.com>
2024-10-23 11:36:27 +0900

    Use tokio (doesn''t work)

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -20,17 +20,7 @@ impl Drop for Foo {
     }
 }
 
-static INTERRUPTED: LazyLock<Arc<AtomicBool>> = LazyLock::new(|| {
-    let interrupted = Arc::new(AtomicBool::new(false));
-    let i = interrupted.clone();
-
-    ctrlc::set_handler(move || {
-        i.store(true, Ordering::SeqCst);
-    })
-    .unwrap();
-
-    interrupted
-});
+static INTERRUPTED: LazyLock<Arc<AtomicBool>> = LazyLock::new(|| Arc::new(AtomicBool::new(false)));
 
 /// @export
 #[savvy]
@@ -38,28 +28,50 @@ fn fun() -> savvy::Result<()> {
     // To check if objects are properly dropped
     let foo = Foo {};
 
-    INTERRUPTED.store(false, Ordering::SeqCst);
+    // Create a runtime.
+    // cf. https://notgull.net/blocking-leaky/
 
-    for i in 0..30 {
-        savvy::r_println!("Iteration {i}");
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    std::thread::spawn({
+        let handle = rt.handle().clone();
 
-        // This works, but why?
-        eval_parse_text("Sys.sleep(0)")?;
+        // Run the handle on this thread, forever.
+        move || handle.block_on(std::future::pending::<()>())
+    });
 
-        if INTERRUPTED.load(Ordering::SeqCst) {
-            savvy::r_println!("Interrupted");
-            unsafe {
-                savvy::unwind_protect(|| {
-                    Rf_onintr();
-                    savvy::NullSexp.into()
-                })?;
+    let _guard = rt.enter();
+
+    rt.spawn(async move {
+        tokio::signal::ctrl_c().await.unwrap();
+        INTERRUPTED.store(true, Ordering::SeqCst);
+    });
+
+    pollster::block_on(async {
+        for i in 0..30 {
+            savvy::r_println!("Iteration {i}");
+
+            // This works, but why?
+            // eval_parse_text("Sys.sleep(0)").unwrap();
+
+            if INTERRUPTED.load(Ordering::SeqCst) {
+                savvy::r_println!("Interrupted");
+                unsafe {
+                    savvy::unwind_protect(|| {
+                        Rf_onintr();
+                        savvy::NullSexp.into()
+                    })
+                    .unwrap();
+                }
+                break;
             }
-            return Ok(());
+            std::thread::sleep(std::time::Duration::from_millis(100));
         }
-        std::thread::sleep(std::time::Duration::from_millis(100));
-    }
 
-    savvy::r_println!("{foo:?}");
+        savvy::r_println!("{foo:?}");
+    });
 
     Ok(())
 }
